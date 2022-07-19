b5d228a9be44055955bebf31571b878be52ec66d
Mulder Oosthuizen <muldero@pepkorit.com>
2022-07-19 02:00:01 +0200

    step-7

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const HomePage(),
       routes: {
